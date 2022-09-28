f0dccc3ab82d7e419afb8d971e445b72a9b1aa1a
rianernesto9@gmail.com <rianernesto9@gmail.com>
2022-09-28 14:52:40 -0300

    Caminho adicionado

--- testeLab.cpp
+++ testeLab.cpp
@@ -3,31 +3,37 @@
 
 using namespace std;
 
-char labMap[25][25] = {{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
-                       {' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
-                       {' ',' ',' ',' ','#','#','#','#','#','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
-                       {' ',' ',' ',' ','#',' ','#',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
-                       {' ',' ',' ',' ','#',' ','#',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
-                       {' ',' ','#','#','#',' ',' ',' ','#','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
-                       {' ',' ',' ',' ','#',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
-                       {' ',' ',' ',' ',' ',' ',' ',' ',' ','#','#','#','#','#','#','#',' ',' ',' ',' ',' ',' ',' ',' ',' '},
-                       {' ',' ',' ',' ','#',' ',' ',' ',' ',' ','#',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' '},
-                       {' ',' ',' ',' ','#',' ',' ',' ',' ',' ','#',' ',' ',' ',' ','#',' ',' ','#',' ',' ',' ',' ',' ',' '},
-                       {' ',' ','#','#','#',' ',' ',' ',' ',' ','#',' ',' ',' ','#','#',' ','#','#','#','#',' ',' ',' ',' '},
-                       {' ',' ','#',' ','#',' ',' ',' ','#','#','#',' ','#',' ','#',' ',' ',' ','#',' ',' ',' ',' ',' ',' '},
-                       {'*','#','#',' ','#',' ',' ',' ',' ',' ','#',' ','#','#','#',' ',' ',' ','#',' ',' ',' ','=',' ',' '},
-                       {' ',' ',' ',' ','#','#',' ',' ',' ',' ','#',' ',' ',' ','#',' ',' ',' ','#',' ',' ',' ','#',' ',' '},
-                       {' ',' ',' ',' ',' ','#','#','#','#','#','#',' ',' ',' ','#',' ','#','#','#',' ',' ',' ','#',' ',' '},
-                       {' ',' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ','#','#','#',' ','#',' ',' ','#','#','#',' '},
-                       {' ',' ',' ',' ',' ',' ',' ','#','#','#',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ','#',' '},
-                       {' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ','#','#','#',' ',' ','#',' '},
-                       {' ',' ',' ',' ',' ',' ',' ',' ','#','#','#','#',' ',' ',' ',' ',' ',' ',' ',' ','#','#','#','#',' '},
-                       {' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' '},
-                       {' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' '},
-                       {' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
-                       {' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
-                       {' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
-                       {' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '}};
+char labMap[25][25] = {{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                       {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                       {' ', ' ', ' ', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                       {' ', ' ', ' ', ' ', '#', ' ', '#', ' ', ' ', '#', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                       {' ', ' ', ' ', ' ', '#', ' ', '#', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                       {' ', ' ', '#', '#', '#', ' ', ' ', '#', '#', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                       {' ', ' ', ' ', ' ', '#', ' ', ' ', '#', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                       {' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', '#', '#', '#', '#', '#', '#', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                       {' ', ' ', ' ', ' ', '#', ' ', '#', '#', '#', ' ', '#', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                       {' ', ' ', ' ', ' ', '#', ' ', ' ', '#', ' ', ' ', '#', ' ', ' ', ' ', ' ', '#', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' '},
+                       {' ', ' ', '#', '#', '#', ' ', '#', '#', ' ', ' ', '#', ' ', ' ', ' ', '#', '#', ' ', '#', '#', '#', '#', ' ', ' ', ' ', ' '},
+                       {' ', ' ', '#', ' ', '#', ' ', ' ', ' ', '#', '#', '#', ' ', '#', ' ', '#', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' '},
+                       {'|', '*', '#', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', ' ', '#', '#', '#', ' ', ' ', ' ', '#', ' ', ' ', ' ', '=', ' ', ' '},
+                       {' ', ' ', ' ', ' ', '#', '#', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', '#', ' ', ' ', ' ', '#', ' ', ' ', ' ', '#', ' ', ' '},
+                       {' ', ' ', ' ', ' ', ' ', '#', '#', '#', '#', '#', '#', ' ', ' ', ' ', '#', ' ', '#', '#', '#', ' ', ' ', ' ', '#', ' ', ' '},
+                       {' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#', '#', '#', ' ', '#', ' ', ' ', '#', '#', '#', ' '},
+                       {' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', '#', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', '#', ' '},
+                       {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', '#', '#', ' ', ' ', '#', ' '},
+                       {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', '#', '#', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', '#', '#', '#', ' '},
+                       {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' '},
+                       {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' '},
+                       {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                       {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                       {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                       {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}};
+
+int carPath[2][200];
+int carPathIndex = 0;
+
+char path[100];
+int pathIndex;
 
 typedef struct Robot
 {
@@ -40,7 +46,7 @@ typedef struct Robot
     Robot()
     {
         initialX = 12;
-        initialY = 0;
+        initialY = 1;
         x = initialX;
         y = initialY;
         direction = 'E';
@@ -66,6 +72,10 @@ Intersections CheckIntersection()
 
 void EastDirection()
 {
+    carPath[0][carPathIndex] = robot.x;
+    carPath[1][carPathIndex] = robot.y;
+    carPathIndex++;
+
     if (labMap[robot.x + 1][robot.y] == ' ' && labMap[robot.x - 1][robot.y] == ' ' && labMap[robot.x][robot.y + 1] == '#')
     {
         labMap[robot.x][robot.y] = '#';
@@ -80,6 +90,8 @@ void EastDirection()
         labMap[robot.x - 1][robot.y] = '*';
         robot.x--;
         robot.direction = 'N';
+        path[pathIndex] = 'L';
+        pathIndex++;
         return;
     }
     else if (labMap[robot.x][robot.y + 1] == '#')
@@ -87,6 +99,8 @@ void EastDirection()
         labMap[robot.x][robot.y] = '#';
         labMap[robot.x][robot.y + 1] = '*';
         robot.y++;
+        path[pathIndex] = 'F';
+        pathIndex++;
         return;
     }
     else if (labMap[robot.x + 1][robot.y] == '#')
@@ -95,19 +109,28 @@ void EastDirection()
         labMap[robot.x + 1][robot.y] = '*';
         robot.x++;
         robot.direction = 'S';
+        path[pathIndex] = 'R';
+        pathIndex++;
         return;
     }
-    else {
+    else
+    {
         labMap[robot.x][robot.y] = '#';
         labMap[robot.x][robot.y - 1] = '*';
         robot.y--;
         robot.direction = 'W';
+        path[pathIndex] = 'B';
+        pathIndex++;
         return;
     }
 }
 
 void WestDirection()
 {
+    carPath[0][carPathIndex] = robot.x;
+    carPath[1][carPathIndex] = robot.y;
+    carPathIndex++;
+
     if (labMap[robot.x + 1][robot.y] == ' ' && labMap[robot.x - 1][robot.y] == ' ' && labMap[robot.x][robot.y - 1] == '#')
     {
         labMap[robot.x][robot.y] = '#';
@@ -122,6 +145,8 @@ void WestDirection()
         labMap[robot.x + 1][robot.y] = '*';
         robot.x++;
         robot.direction = 'S';
+        path[pathIndex] = 'L';
+        pathIndex++;
         return;
     }
     else if (labMap[robot.x][robot.y - 1] == '#')
@@ -129,6 +154,8 @@ void WestDirection()
         labMap[robot.x][robot.y] = '#';
         labMap[robot.x][robot.y - 1] = '*';
         robot.y--;
+        path[pathIndex] = 'F';
+        pathIndex++;
         return;
     }
     else if (labMap[robot.x - 1][robot.y] == '#')
@@ -137,19 +164,28 @@ void WestDirection()
         labMap[robot.x - 1][robot.y] = '*';
         robot.x--;
         robot.direction = 'N';
+        path[pathIndex] = 'R';
+        pathIndex++;
         return;
     }
-    else {
+    else
+    {
         labMap[robot.x][robot.y] = '#';
         labMap[robot.x][robot.y + 1] = '*';
         robot.y++;
         robot.direction = 'E';
+        path[pathIndex] = 'B';
+        pathIndex++;
         return;
     }
 }
 
 void NorthDirection()
 {
+    carPath[0][carPathIndex] = robot.x;
+    carPath[1][carPathIndex] = robot.y;
+    carPathIndex++;
+
     if (labMap[robot.x][robot.y + 1] == ' ' && labMap[robot.x][robot.y - 1] == ' ' && labMap[robot.x - 1][robot.y] == '#')
     {
         labMap[robot.x][robot.y] = '#';
@@ -164,6 +200,8 @@ void NorthDirection()
         labMap[robot.x][robot.y - 1] = '*';
         robot.y--;
         robot.direction = 'W';
+        path[pathIndex] = 'L';
+        pathIndex++;
         return;
     }
     else if (labMap[robot.x - 1][robot.y] == '#')
@@ -171,6 +209,8 @@ void NorthDirection()
         labMap[robot.x][robot.y] = '#';
         labMap[robot.x - 1][robot.y] = '*';
         robot.x--;
+        path[pathIndex] = 'F';
+        pathIndex++;
         return;
     }
     else if (labMap[robot.x][robot.y + 1] == '#')
@@ -179,19 +219,28 @@ void NorthDirection()
         labMap[robot.x][robot.y + 1] = '*';
         robot.y++;
         robot.direction = 'E';
+        path[pathIndex] = 'R';
+        pathIndex++;
         return;
     }
-    else {
+    else
+    {
         labMap[robot.x][robot.y] = '#';
         labMap[robot.x + 1][robot.y] = '*';
         robot.x++;
         robot.direction = 'S';
+        path[pathIndex] = 'B';
+        pathIndex++;
         return;
     }
 }
 
 void SouthDirection()
 {
+    carPath[0][carPathIndex] = robot.x;
+    carPath[1][carPathIndex] = robot.y;
+    carPathIndex++;
+
     if (labMap[robot.x][robot.y + 1] == ' ' && labMap[robot.x][robot.y - 1] == ' ' && labMap[robot.x + 1][robot.y] == '#')
     {
         labMap[robot.x][robot.y] = '#';
@@ -206,6 +255,8 @@ void SouthDirection()
         labMap[robot.x][robot.y + 1] = '*';
         robot.y++;
         robot.direction = 'E';
+        path[pathIndex] = 'L';
+        pathIndex++;
         return;
     }
     else if (labMap[robot.x + 1][robot.y] == '#')
@@ -213,6 +264,8 @@ void SouthDirection()
         labMap[robot.x][robot.y] = '#';
         labMap[robot.x + 1][robot.y] = '*';
         robot.x++;
+        path[pathIndex] = 'F';
+        pathIndex++;
         return;
     }
     else if (labMap[robot.x][robot.y - 1] == '#')
@@ -221,13 +274,18 @@ void SouthDirection()
         labMap[robot.x][robot.y - 1] = '*';
         robot.y--;
         robot.direction = 'W';
+        path[pathIndex] = 'R';
+        pathIndex++;
         return;
     }
-    else {
+    else
+    {
         labMap[robot.x][robot.y] = '#';
         labMap[robot.x - 1][robot.y] = '*';
         robot.x--;
         robot.direction = 'N';
+        path[pathIndex] = 'B';
+        pathIndex++;
         return;
     }
 }
@@ -266,6 +324,16 @@ bool IsVictory()
     return false;
 }
 
+bool CheckIfCarPath(int x, int y) {
+    int i;
+    for(i = 0; i < carPathIndex; i++) {
+        if(carPath[0][i] == x && carPath[1][i] == y) {
+            return true;
+        }
+    }
+    return false;
+}
+
 void PrintMap()
 {
     int i, j;
@@ -273,10 +341,15 @@ void PrintMap()
     {
         for (j = 0; j < 25; j++)
         {
-            if(labMap[i][j] == '*') {
+            if (labMap[i][j] == '*')
+            {
                 SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
             }
-            else {
+            else if(CheckIfCarPath(i, j)) {
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
+            }
+            else
+            {
                 SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
             }
             cout << labMap[i][j];
@@ -285,13 +358,93 @@ void PrintMap()
     }
 }
 
+void PrintPath(bool isOrganized)
+{   
+    if(isOrganized)
+        cout << "Correct Path = [ ";
+    else
+        cout << "Path = [ ";
+    int i;
+    for (i = 0; i < pathIndex; i++)
+    {
+        cout << path[i] << " ";
+    }
+    cout << "]\n\n";
+}
+
+void OrganizePath(int position) {
+    int i;
+    for(i = position + 2; i < pathIndex - 1; i++) {
+        path[i - 1] = path[i + 1];
+    }
+    pathIndex -= 2;
+}
+
+void CheckPath()
+{
+    char tempPath[100];
+    int i = 0;
+    while (true)
+    {
+        if(i + 2 > pathIndex)
+            break;
+
+        char firstChar = path[i];
+        char secondChar = path[i + 1];
+        char thirdChar = path[i + 2];
+
+        if(firstChar == 'L' && secondChar == 'B' && thirdChar == 'R') {
+            path[i] = 'B';
+            OrganizePath(i);
+            i = 0;
+            continue;
+        }
+        if(firstChar == 'L' && secondChar == 'B' && thirdChar == 'F') {
+            path[i] = 'R';
+            OrganizePath(i);
+            i = 0;
+            continue;
+        }
+        if(firstChar == 'R' && secondChar == 'B' && thirdChar == 'L') {
+            path[i] = 'B';
+            OrganizePath(i);
+            i = 0;
+            continue;
+        }
+        if(firstChar == 'F' && secondChar == 'B' && thirdChar == 'L') {
+            path[i] = 'R';
+            OrganizePath(i);
+            i = 0;
+            continue;
+        }
+        if(firstChar == 'F' && secondChar == 'B' && thirdChar == 'F') {
+            path[i] = 'B';
+            OrganizePath(i);
+            i = 0;
+            continue;
+        }
+        if(firstChar == 'L' && secondChar == 'B' && thirdChar == 'L') {
+            path[i] = 'F';
+            OrganizePath(i);
+            i = 0;
+            continue;
+        }
+
+        i++;
+    }
+}
+
 int main()
 {
     PrintMap();
-    while(!IsVictory()){
+    while (!IsVictory())
+    {
         Walk();
         system("cls || clear");
         PrintMap();
     }
+    PrintPath(false);
+    CheckPath();
+    PrintPath(true);
     return 0;
 }
